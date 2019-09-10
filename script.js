246627cf645f5c0a1228c63eaaccb3684fa44d39
Kevaughn <kevaughnc04@gmail.com>
2019-09-09 22:03:01 -0500

    Color divs to black with mouseover

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const size = Number(prompt("What size grid?"));
 const container = document.querySelector("#container");
 for (let i = 0; i < Math.pow(size, 2); i++){
     const div = document.createElement("div");
+    div.classList.add("grid");
     div.setAttribute("style", `border: 1px solid black; float: left; width: ${percentage(size)}%; height: ${percentage(size)}%`);
     container.appendChild(div);
 }
+container.addEventListener("mouseover", (e) => {
+    console.log(e.target);
+    e.target.style.backgroundColor = "black";
+    
+    
+});
 
 function percentage(number){
-    return ((1/number) * 100) -1;
+    return ((1/number) * 100) - 1;
 }
